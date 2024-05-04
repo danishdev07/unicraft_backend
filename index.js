eb95ba49a992a4e1387fc8562123886b5095a4b4
danishdev07 <danish.dev07@gmail.com>
2024-05-04 18:36:26 +0530

    connection my sql basic done

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const mysql = require('mysql');
 const hostname = '127.0.0.1';
 const port = 3000;
 const app = express();
-// const connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'your_username',
-//   password: 'your_password',
-//   database: 'your_database_name'
-// });
-// // Connect to the database
-// connection.connect((err) => {
-//     if (err) {
-//       console.error('Error connecting to MySQL:', err.message);
-//       return;
-//     }
-//     console.log('Connected to MySQL database');
-//   });
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'your_username',
+  password: 'your_password',
+  database: 'your_database_name'
+});
+// Connect to the database
+connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL:', err.message);
+      return;
+    }
+    console.log('Connected to MySQL database');
+  });
 
 // Start the server
 app.listen(port, hostname, () => {
